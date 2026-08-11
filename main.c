4f8240bf5e089e8ba5a17264c308692be4933bb4
kieronisME <Kierontekumu2@gmail.com>
2026-08-11 16:06:31 +0100

    window opens

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <GL/freeglut.h>  
 #include <math.h>
 
-int main (){
-    printf("is this thing on???");
+//window dimentions 
+#define WIDTH  1024
+#define HEIGHT 512
 
+
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glutSwapBuffers();
+    
+}
+
+void init(){
+
+    glClearColor(0.5, 0.5, 0.5, 0);
+    gluOrtho2D(0, WIDTH, HEIGHT, 0);
+}
+
+
+
+int main (int argc, char* argv[]){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(WIDTH, HEIGHT);
+    glutCreateWindow("wizard casting rays");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
     return 0;
 }
